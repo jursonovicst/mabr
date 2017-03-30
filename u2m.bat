8113c413d7c09e5c405d92bd300296e3f56ccd2a
t.jursonovics <tamas.jursonovics@telekom.de>
2017-03-30 13:28:50 +0200

    update nex syntax

--- u2m.bat
+++ u2m.bat
@@ -1,1 +1,1 @@
-c:\Python27\python.exe C:\Users\A9423340\PycharmProjects\mabr\u2m.py --severity DEBUG --proxy http://10.35.3.35:3128 --config ard.conf http://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestARD.mpd
+c:\Python27\python.exe C:\Users\A9423340\PycharmProjects\mabr\u2m.py --severity DEBUG --proxy http://10.35.3.35:3128 ard.conf
