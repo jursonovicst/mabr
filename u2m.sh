fc5457abe3eb9693139a68801a788876badb6f21
jursonovicst <jursonovicst@gmail.com>
2017-04-21 15:41:51 +0200

    config

--- u2m.sh
+++ u2m.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python2.7 u2m.py --severity DEBUG --config ard.conf http://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestARD.mpd
+/usr/bin/python2.7 u2m.py --severity DEBUG ard.conf
