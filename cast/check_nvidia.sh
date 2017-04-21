0e3f58adfa6888c7a63297ed58f0ff00b11510e6
cdn <cdn@mabr-headend.atu0.org>
2017-04-21 16:47:20 +0200

    move GPU numer into the name

--- cast/check_nvidia.sh
+++ cast/check_nvidia.sh
@@ -2,11 +2,12 @@
 
 statfile="/tmp/nvidiastat"
 
-nvidia-smi dmon -i 0 -c 1 >$statfile
+nvidia-smi dmon -c 1 >$statfile
 
 titles=( $(head -n 1 $statfile) )
 values=( $(tail -n 1 $statfile) )
 
-status="${titles[1]}=${values[0]}|${titles[2]}=${values[1]}|${titles[3]}=${values[2]}|${titles[4]}=${values[3]}|${titles[5]}=${values[4]}|${titles[6]}=${values[5]}|${titles[7]}=${values[6]}|${titles[8]}=${values[7]}|${titles[9]}=${values[8]}"
-echo "0 nvidia $status - OK $status"
+status="${titles[2]}=${values[1]}|${titles[3]}=${values[2]}|${titles[4]}=${values[3]}|${titles[5]}=${values[4]}|${titles[6]}=${values[5]}|${titles[7]}=${values[6]}|${titles[8]}=${values[7]}|${titles[9]}=${values[8]}"
+name=nvidia_gpu${values[0]}
+echo "0 $name $status - OK $status"
 
