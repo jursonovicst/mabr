6e8744feb9d634a10d2b22328635547f0daf1b9d
jursonovicst <jursonovicst@gmail.com>
2017-10-09 12:20:46 +0200

    rename seqmin/max and changed site to 2 bytes

--- rtpext/rtpmabr.py
+++ rtpext/rtpmabr.py
@@ -2,21 +2,21 @@ import imp
 try:
   imp.find_module('dpkt')
 except ImportError:
-  print("This scrypt requires dpkt.rtp python library, please install python-dpkt!")
-  exit(1)
+    raise Exception("This scrypt requires dpkt.rtp python library, please install python-dpkt!")
 from dpkt.rtp import RTP
 
 import socket, struct
 
 
+# RTP packet to hold slices of fragments. Not the last fragment
 class RTPMABRDATA(RTP):
     ID = 0xabba
 
     __hdr__ = RTP.__hdr__ + (
-        ('id', 'H', ID),
-        ('length', 'H', 2),
-        ('bytemin', 'I', 0),
-        ('bytemax', 'I', 0),
+        ('id', 'H', ID),        # RTPMABRDATA extension header identifier
+        ('length', 'H', 2),     # RTPMABRDATA extension header length, fix value, to be updated, if header changes!
+        ('bytemin', 'I', 0),    # slice's starting byte offset in the fragment
+        ('bytemax', 'I', 0),    # slice's ending byte offset in the fragment
     )
 
     def unpack(self, buf):
@@ -25,27 +25,26 @@ class RTPMABRDATA(RTP):
             raise Exception("Invalid RTPMABRDATA packet format")
 
 
-
+# RTP packet to hold the last slices of fragments
 class RTPMABRSTITCHER(RTPMABRDATA):
-    ID = 0xbaab
+    ID = 0xbaab                         # RTPMABRSTITCHER extension header identifier
 
-    __hdr__ = RTPMABRDATA.__hdr__ + (
-        ('seqmin', 'I', 0),
-        ('seqmax', 'I', 0),
-        ('chunknumber', 'I', 0),
+    __hdr__ = RTPMABRDATA.__hdr__ + (   # same as above
+        ('burstseqmin', 'H', 0),        # ???
+        ('burstseqmax', 'H', 0),        # ???
+        ('chunknumber', 'I', 0),        # ???
     )
 
     def __init__(self, rtpmabrdata=None):
         super(RTPMABRSTITCHER, self).__init__()
         if rtpmabrdata is not None:
-            self._length=5
+            self._length=4
 
             self._type = rtpmabrdata._type
             self.seq = rtpmabrdata.seq
             self.ts = rtpmabrdata.ts
             self.ssrc = rtpmabrdata.ssrc
             self.id = RTPMABRSTITCHER.ID
-            self.length = 5
             self.bytemin = rtpmabrdata.bytemin
             self.bytemax = rtpmabrdata.bytemax
             self.csrc = rtpmabrdata.csrc
