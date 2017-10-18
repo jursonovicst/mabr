35028e182fca06ad892aedb0b6d81cec019e6add
jursonovicst <jursonovicst@gmail.com>
2017-10-18 15:03:46 +0200

    cleanup

--- m2u/stitcher.py
+++ m2u/stitcher.py
@@ -1,89 +1,69 @@
-import threading
-import Queue
-import signal, os
 from channels import *
 from rtpext import RTPMABRSTITCHER
 
-import imp
-
-import imp
-try:
-    imp.find_module('memcache')
-except ImportError:
-    raise Exception("This scrypt requires memcache python library, please install python-memcache!")
-import memcache
+import threading
+import Queue
 
 
 class Stitcher(threading.Thread):
+
     _stitcherqueue = Queue.Queue(30)
     _timeout = 0.1
 
-
     @classmethod
     def stitch(cls, ssrc, burstseqfirst, burstseqlast, chunknumber, checksum, logger):
-        logger.debug('Initiate stitching for: ssrc=%s, rtpseq=%d-%d, chunknumber=%d, checksum=%04x' % (ssrc, burstseqfirst, burstseqlast, chunknumber, checksum))
+        logger.debug('Initiate stitching for: ssrc=%s, rtpseq=%d-%d, chunknumber=%d, checksum=%s' % (ssrc, burstseqfirst, burstseqlast, chunknumber, RTPMABRSTITCHER.checksum2str(checksum)))
         cls._stitcherqueue.put_nowait((ssrc, burstseqfirst, burstseqlast, chunknumber, checksum))
 
     def __init__(self, group=None, target=None, name=None, args=(), kwarggs=None):
         threading.Thread.__init__(self, group, target, name, args, kwarggs)
 
         self._logger = args[0]
-        self._memcached = memcache.Client([args[1]])
+        self._memdb = args[1]
 
         self._run = True
 
     def run(self):
         while self._run:
             try:
-                #get stitching job
+                # get stitching job
                 ssrc, burstseqfirst, burstseqlast, chunknumber, checksum = Stitcher._stitcherqueue.get(True, Stitcher._timeout)
 
-                #find my channel
-                #find my stream
-
-
-                memcachedkeys = []
+                memdbkeys = []
                 if burstseqlast >= burstseqfirst:
-                    for seq in range(burstseqfirst, burstseqlast):
-                        memcachedkeys.append(Slice.getmemcachedkey(ssrc,seq))
+                    for seq in range(burstseqfirst, burstseqlast + 1):
+                        memdbkeys.append(Slice.getmemcachedkey(ssrc, seq))
 
-                #handle RTP seq overflow
+                # handle RTP seq overflow
                 else:
-                    for seq in range(burstseqfirst, 2 ** 16-1):
-                        memcachedkeys.append(Slice.getmemcachedkey(ssrc,seq))
-                    for seq in range(0, burstseqlast):
-                        memcachedkeys.append(Slice.getmemcachedkey(ssrc,seq))
+                    for seq in range(burstseqfirst, 2 ** 16):
+                        memdbkeys.append(Slice.getmemcachedkey(ssrc, seq))
+                    for seq in range(0, burstseqlast + 1):
+                        memdbkeys.append(Slice.getmemcachedkey(ssrc, seq))
 
                 # get all slices from memcached
-                ret = self._memcached.get_multi(memcachedkeys)
+                ret = self._memdb.get_multi(memdbkeys)
 
 
                 chunk = ''
-                for key in memcachedkeys:
+                for key in memdbkeys:
                     try:
                         chunk += ret[key]
                     except KeyError:
-                        #self._logger.warning("Packet rtpseq=%s has been lost --> retransmission (not yet implemented)!" % seq)
+                        self._logger.error("Packet %s has been lost --> retransmission (not yet implemented)!" % key)
                         continue
 
-                # calculate packet loss rate
-                packetlossrate = 1.0-(float(len(ret.keys())) / (len(memcachedkeys)+0.0000001))
-
                 # check fragment integrity
-                if not RTPMABRSTITCHER.validateChecksum(chunk, checksum):
-                    self._logger.warning("Fragment chunknumber=%d stitching failed, invalid checksum (packet loss rate: %d%%)!" % (chunknumber, packetlossrate * 100))
+                if len(memdbkeys) != len(ret.keys()):           # or not RTPMABRSTITCHER.validateChecksum(chunk, checksum) :
+                    self._logger.warning("ssrc: %d chunknumber: %d stitching failed, len: %dB, checksum: '%s' is invalid, expected: '%s' (plr: %d/%d)!" % (ssrc, chunknumber, len(chunk), RTPMABRSTITCHER.checksum2str(RTPMABRSTITCHER.checksum(chunk)), RTPMABRSTITCHER.checksum2str(checksum), len(memdbkeys)-len(ret.keys()), len(memdbkeys)))
                     continue
 
-                if packetlossrate < 0.01:
-                    self._logger.debug("Fragment chunknumber=%d has been stitched, packet loss rate: %d%%" % (chunknumber, packetlossrate * 100))
-                elif packetlossrate < 0.05:
-                    self._logger.warning("Fragment chunknumber=%d has been stitched, packet loss rate: %d%%" % (chunknumber, packetlossrate * 100))
-                else:
-                    self._logger.error("Fragment chunknumber=%d has been stitched, packet loss rate: %d%%" % (chunknumber, packetlossrate * 100))
+                self._logger.debug("ssrc: %d chunknumber: %d has been stitched, packet loss rate: %d/%d" % (ssrc, chunknumber, len(memdbkeys)-len(ret.keys()), len(memdbkeys)))
 
 
                 # store in memcached
-                self._memcached.set(Chunk.getmemcachedkey(ssrc,chunknumber), chunk)
+                if not self._memdb.set(Chunk.getmemcachedkey(ssrc, chunknumber), chunk):
+                    self._logger.warning("Cannot store stitched object in memcached!")
 
 
             except Queue.Empty:
